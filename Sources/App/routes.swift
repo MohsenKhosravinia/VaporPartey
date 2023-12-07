0b13b8baceee54b169132967596482719210661d
Mohsen Khosravinia <khosravinia.mohsen@gmail.com>
2023-12-08 01:29:22 +0330

    Add the first route :)

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,9 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("hello", ":name") { req async -> String in
+        let name = try! req.parameters.require("name", as: String.self)
+        return "hello \(name)"
+    }
 }
